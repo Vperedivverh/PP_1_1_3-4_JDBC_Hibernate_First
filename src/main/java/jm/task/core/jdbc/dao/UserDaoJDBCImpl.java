3455b5ed057eb98da495fa3a7af991e714fcbf96
Vperedivverh <105232340+Vperedivverh@users.noreply.github.com>
2022-07-26 11:12:44 +0300

    добавил rollbacks

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,10 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -9,27 +12,138 @@ public class UserDaoJDBCImpl implements UserDao {
 
     }
 
+    Connection connection = Util.getConnection();
+
     public void createUsersTable() {
+        String query = "CREATE TABLE IF NOT EXISTS users " +
+                "(id INT NOT NULL AUTO_INCREMENT,  name VARCHAR(100), lastName VARCHAR(100), age INT, PRIMARY KEY (id))";
+        try (
+                Statement statement = connection.createStatement()
+        ) {
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+
+
+//            System.out.println("проблема с созданием коннекта");
+            e.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+
 
     }
 
     public void dropUsersTable() {
+        String query = "DROP TABLE IF EXISTS users";
+        try (
+                Statement statement = connection.createStatement()) {
+            statement.executeUpdate(query);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+
 
     }
 
+    //    public void saveUser(String name, String lastName, byte age) {
+//        String query = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)";
+//        try (Connection connection = Util.getConnection();
+//             PreparedStatement preparedStatement = connection.prepareStatement(query)
+//        ) {
+//            preparedStatement.setString(1, name);
+//            preparedStatement.setString(2, lastName);
+//            preparedStatement.setByte(3, age);
+//            preparedStatement.executeUpdate();
+//            System.out.println("User с именем – " + name + " добавлен в базу данных");
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
     public void saveUser(String name, String lastName, byte age) {
-
+        try (PreparedStatement prepareStatement = connection.prepareStatement("INSERT users(name,lastname,age) values (?,?,?)");) {
+            prepareStatement.setString(1, name);
+            prepareStatement.setString(2, lastName);
+            prepareStatement.setByte(3, age);
+            prepareStatement.executeUpdate();
+            System.out.println("User с именем – " + name + " добавлен в базу данных");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
     public void removeUserById(long id) {
+        String query = "DELETE FROM users WHERE id = ?";
+        try (
+                PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
 
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList = new ArrayList<>();
+        String query = "SELECT id, name, lastName, age FROM users";
+        try (
+                Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                String lastName = resultSet.getString("lastName");
+                byte age = resultSet.getByte("age");
+                User user = new User(name, lastName, age);
+                user.setId(id);
+                userList.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        return userList;
     }
 
     public void cleanUsersTable() {
+        String query = "DELETE FROM users";
+        try (
+                Statement statement = connection.createStatement()) {
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            e.printStackTrace();
+        }
 
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,31 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/mysql";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "root";
+
+    public static Connection getConnection() {
+
+
+        Connection connection = null;
+
+        try {
+
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            if (!connection.isClosed()) {
+//                System.out.println("use u poryadke,shef!");
+            }
+        } catch (Exception e) {
+            System.out.println("net kontakta(");
+            e.printStackTrace();
+
+        }
+        return connection;
+    }
 }
